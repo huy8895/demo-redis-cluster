392b0de2cd8d9da3d33e1c505d4a10c0e0017ea2
huytvdev <huy8895@gmail.com>
2024-03-09 23:31:51 +0700

    init

---
+++ src/main/java/com/example/demorediscluster/DemoRedisClusterApplication.java
@@ -0,0 +1,13 @@
+package com.example.demorediscluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRedisClusterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoRedisClusterApplication.class, args);
+  }
+
+}
